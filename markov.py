a38b9dafaa8162d729757d4f063b81ea63d019b4
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-10 15:32:10 -0700

    more feature work

--- markov.py
+++ markov.py
@@ -4,14 +4,17 @@ from random import choice
 import sys
 
 
-def open_and_read_file(file_path):
+def open_and_read_file(file_paths):
     """Take file path as string; return text as string.
 
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
-    with open(file_path) as long_string:
-        text_string = long_string.read()
+    text_string = ""
+    for file_path in file_paths:
+        with open(file_path) as long_string:
+            text_string = text_string + long_string.read()
+
     return text_string
 
 
@@ -43,7 +46,6 @@ def make_chains(text_string, number_words):
     chains = {}
     text_list = text_string.split()
 
-
     for i in range(len(text_list)-number_words):
         word_combo = tuple(text_list[i:i+number_words])
         chains[word_combo] = chains.get(word_combo, []) + [text_list[i+number_words]]
@@ -58,44 +60,81 @@ def make_chains(text_string, number_words):
 def make_text(chains, number_words):
     """Return text from chains."""
 
-
     # from tuple / link key
     # randomly pick an item from its list of values
-    # adds last two words of the k-v pair as new current key..do until the key doesn't exist
     current_key = choice(chains.keys())
-    while current_key[0].title() != current_key[0]:
+    # while first word in key is not proper, keep picking a random key.
+    while current_key[0].title() != current_key[0] or current_key[0] == "--":
         current_key = choice(chains.keys())
 
     words = list(current_key)
-
-    # print "this is the current key", current_key
-    # print "these are the words:", words
-    
-    while current_key in chains.keys() or len(words) <= 140:
+    current_length = len(" ".join(words))
+    # adds last number_words of the k-v pair as new current key
+    # do until the key doesn't exist
+    while current_key in chains.keys() and current_length <= 2000:
         last_word = choice(chains[current_key])
         words.append(last_word)
-        current_key = tuple(words[-number_words:])          
+        current_length = len(" ".join(words))
+        current_key = tuple(words[-number_words:])
     return " ".join(words)
 
 
-input_path = sys.argv[1]
-files_list = sys.argv
+def output_file(random_text):
+    """Create/edit an output file, adding text input to the file.
+
+    Arg:
+    Param (string): a random string of data
+
+    Returns nothing
+    """
+    output = open("markov-story.out", "w")
+    output.write(random_text)
+    output.close()
+    print "Your text is now saved into the output file markov-story."
 
 
+def give_user_choice_of_ngrams(text_string):
+    """Shows n-grams from 2-10 and prompts user to select one to return."""
+    # create dictionary of all ngram options and print so the user can choose.
+    ngram_options = {}
+    for i in range(2, 11):
+        ngram_chains = make_chains(text_string, i)
+        ngram_options[i] = make_text(ngram_chains, i)
 
-number_words = 10
+        print "This is a story from a {}-gram:".format(i)
+        print "{}".format(ngram_options[i])
+        print "-------------------"
 
+    # prompt user to select which n-gram they like
+    # validate user input for range and integer
+    print "Which ngram do you want to save?"
+    while True:
+        user_choice = raw_input("Enter a number between 2 and 10?\n")
+        try:
+            user_choice = int(user_choice)
+        except ValueError:
+            print "You need to enter a number (between 2 and 10). Try again!"
+            continue
+        if user_choice not in range(2, 11):
+            print "You did not enter a number between 2 and 10, try again."
+            continue
+        break
+    print "You selected {}-gram.".format(
+        user_choice)
+
+    return ngram_options[user_choice]
+
+
+input_path = sys.argv[1:]
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
-# Get a Markov chain
-chains = make_chains(input_text, number_words)
+# not using below with new feature adds
+# number_words = 2
+# # Get a Markov chain
+# chains = make_chains(input_text, number_words)
+# # Produce random text
+# random_text = make_text(chains, number_words)
 
-#print chains
-
-# Produce random text
-random_text = make_text(chains, number_words)
-
-print random_text
-# print sys.argv
-# print 'these are the text files inputted on the command line', input_path
+user_choice = give_user_choice_of_ngrams(input_text)
+story = output_file(user_choice)
